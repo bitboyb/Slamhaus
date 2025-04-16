a4100c24447569f5192ca787646f54f37f51a4d6
Syed Assad Abbas Bokhari <62619775+bitboyb@users.noreply.github.com>
2025-04-16 21:36:06 +0100

    seo namespace is garbage, needs a refactor

--- src/interpreter/seo.hpp
+++ src/interpreter/seo.hpp
@@ -2,22 +2,43 @@
 #define SEO_HPP
 
 #include <string>
+#include "../core/config.hpp"
 
 namespace SEO
 {
+    struct SEOData
+    {
+        std::string siteTitle;
+        std::string siteDescription;
+        std::string siteUrl;
+        std::string siteFavicon;
+        std::string pageDescription;
+        std::string pageKeywords;
+    };
+
+    static std::string siteTitle;
+    static std::string siteDescription;
+    static std::string siteUrl;
+    static std::string siteFavicon;
+    static std::string pageDescription;
+    static std::string pageKeywords;
+
     bool IsSiteLine(const std::string &line);
     bool IsPageLine(const std::string &line);
 
     std::string ProcessSiteLine(const std::string &line);
     std::string ProcessPageLine(const std::string &line);
+
+    SEO::SEOData GetDefault(Config::ConfigINI &ini);
+
+    // Delete this shit
+
     std::string GetSiteFavicon();
     std::string GetSiteUrl();
-    
     void SetSiteTitle(const std::string &title);
     void SetSiteDescription(const std::string &desc);
     void SetSiteFavicon(const std::string &path);
     void SetSiteUrl(const std::string &url);
-
     std::string GetSiteTitle();
     std::string GetSiteDescription();
     std::string GetPageDescription();
