691297df857cbffe75de31034d689912bb094b50
Syed Assad Abbas Bokhari <62619775+bitboyb@users.noreply.github.com>
2025-05-02 22:35:11 +0100

    clearing cached content

--- scripts/rename_prefix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script renames files by removing the "@3x-" prefix.
-
-shopt -s nullglob  # If no files match, do nothing.
-for file in @3x-*; do
-    newname="${file#@3x-}"
-    echo "Renaming '$file' to '$newname'"
-    mv "$file" "$newname"
-done
